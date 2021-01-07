98d694ff43a45c0fff14f0b858b6ab765802d5d1
Akhilasai14 <akhilasai.bejjarapu@gmail.com>
2021-01-07 19:37:25 +0530

    variable-hosting.js

--- variable-hosting.js
+++ variable-hosting.js
@@ -1,3 +1,31 @@
-x=2;
+/*x=2;
 console.log(x);
-var x;
+var x;*/
+
+// In Variable Hoisting , variable is declared at the top...//
+
+/*function f1(){
+    a=9;
+    b=10;
+    c=a*b;
+    console.log(c);
+    var a;
+    var b;
+    var c;
+}
+f1(); // prints the output of function.(local variable)*/
+
+
+var a;
+var b;
+var c;
+function f1(){
+    a=10;
+    b=8;
+    c=a%b;
+    console.log(c);
+}
+f1();
+
+
+
